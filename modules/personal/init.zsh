22f96d38126fc984f67ccccdb072a219cd3a3e0a
Muyang Li <muyang@patreon.com>
2023-10-30 17:20:09 -0700

    pytest

--- modules/personal/init.zsh
+++ modules/personal/init.zsh
@@ -11,6 +11,18 @@ alias gsl="clear && clear && git log --graph --pretty=format:'%Cred%h%Creset %C(
 alias prf='cd ~/workspace/patreon_react_features'
 alias ppy='cd ~/workspace/patreon_py'
 
+alias rs_prf='rdev new patreon_react_features --dotfiles muyang --name prf --no-set-default'
+alias rs_ppy='rdev new patreon_py --dotfiles muyang --name ppy --set-default'
+alias rs_tail="rdev services tail '*'"
+alias rs_tail_prf="rdev services tail '*' --container-name prf"
+alias rs_restart_prf='rdev services restart ssr react next --container-name prf'
+alias rs_restart_ppy='rdev services restart --container-name ppy'
+alias rs_restart='rs_restart_prf && rs_restart_ppy'
+alias rs_recreate='rdev rm prf --no-check && rdev rm ppy --no-check && rdev_prf && rdev_ppy'
+
+alias pytest='/home/dev/patreon_py/bin/rdev/pytest'
+
+
 
 ENABLE_TYPECHECK_PATREON_PY=1
 
